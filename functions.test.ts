a12193c6e3797d83ec038bc28a3568231ff37f31
Hunter Birch <hunter.macka.birch@gmail.com>
2022-07-01 16:12:39 -0600

    function tests work properly

--- functions.test.ts
+++ functions.test.ts
@@ -1,11 +1,19 @@
 const {shuffleArray} = require('./utils')
 
-describe('shuffleArray should', () => {
-    it("returns an array", () => {
-        expect(shuffleArray).toReturn()
-    });
+const testArr1 = [1,2,3,4]
+const testArr2 = []
+const testArr3 = ['Mohana', 'you', 'ugly']
 
-    it("return array should be shuffled", () => {
-        expect(shuffleArray).toContainEqual(shuffleArray)
+describe('shuffleArray should', () => {
+    test('return an array', () => {
+        expect(Array.isArray(shuffleArray(testArr1))).toBe(true)
+        expect(Array.isArray(shuffleArray(testArr2))).toBe(true)
+        expect(Array.isArray(shuffleArray(testArr3))).toBe(true)
+    })
+
+    test('have output array same length as input array', () => {
+        expect(shuffleArray(testArr1)).toHaveLength(testArr1.length)
+        expect(shuffleArray(testArr2)).toHaveLength(testArr1.length)
+        expect(shuffleArray(testArr3)).toHaveLength(testArr1.length)
     })
 });
